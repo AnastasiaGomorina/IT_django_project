b6fe42403137e65d567371ea99171b13631d9e1f
Анастасия <nastyagomorinalove@gmail.com>
2025-04-09 01:41:06 +0300

    Выполнен 4 шаг: подключена статика

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -123,6 +123,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Если у тебя будет общая папка статики:
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
