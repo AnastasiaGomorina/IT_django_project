e504421399fb34a4985369347d48b48c852d75ea
Анастасия <nastyagomorinalove@gmail.com>
2025-04-21 22:53:42 +0300

    Реализован функционал pages

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,4 +1,9 @@
+
 from django.shortcuts import render
+
+def index(request):
+    context = {"posts": posts}
+    return render(request, "blog/index.html", context)
 
 def about(request):
     return render(request, 'pages/about.html')
