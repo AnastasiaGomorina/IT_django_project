2e0c5cb6509db0e2a3d4eb81cbe4d633b9fad6db
Анастасия <nastyagomorinalove@gmail.com>
2025-04-22 00:03:09 +0300

    Добавлен функционал приложения blog

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,34 +1,192 @@
-from django.shortcuts import render, get_object_or_404
-from django.utils import timezone
-from .models import Category, Post
+from datetime import datetime
+
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+from django.db.models import Count
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .forms import PostForm, CommentForm, UserForm
+from .models import Post, Category, User, Comment
+
+
+NUMBER_OF_PAGINATOR_PAGES = 10
+
+
+def get_posts(**kwargs):
+    """Отфильтрованное получение постов"""
+    return Post.objects.select_related(
+        'category',
+        'location',
+        'author'
+    ).annotate(comment_count=Count('comments')
+               ).filter(**kwargs).order_by('-pub_date')
+
+
+def get_paginator(request, queryset,
+                  number_of_pages=NUMBER_OF_PAGINATOR_PAGES):
+    """Представление queryset в виде пагинатора,
+       по N-шт на странице"""
+    paginator = Paginator(queryset, number_of_pages)
+    page_number = request.GET.get('page')
+    return paginator.get_page(page_number)
+
 
 def index(request):
-    # Главная страница: 5 последних публикаций с фильтрами
-    post_list = Post.objects.filter(
-        is_published=True,                  # Публикация опубликована
-        pub_date__lte=timezone.now(),       # Дата публикации не позже текущего времени
-        category__is_published=True         # Категория опубликована
-    ).order_by('-pub_date')[:5]             # Сортировка по убыванию даты, первые 5
-    return render(request, 'blog/index.html', {'post_list': post_list})
+    """Главная страница / Лента публикаций"""
+    posts = get_posts(
+        is_published=True,
+        category__is_published=True,
+        pub_date__lte=datetime.now())
+    page_obj = get_paginator(request, posts)
+    context = {'page_obj': page_obj}
+    return render(request, 'blog/index.html', context)
 
-def post_detail(request, id):
-    # Страница отдельной публикации с фильтрами и ошибкой 404
-    post = get_object_or_404(Post.objects.filter(
-        is_published=True,                  # Публикация опубликована
-        pub_date__lte=timezone.now(),       # Дата публикации не позже текущего времени
-        category__is_published=True         # Категория опубликована
-    ), id=id)                               # Фильтр по первичному ключу
-    return render(request, 'blog/detail.html', {'post': post})
 
 def category_posts(request, category_slug):
-    # Страница категории: посты с фильтрами и ошибка 404 для неопубликованной категории
-    category = get_object_or_404(Category, slug=category_slug, is_published=True)
-    post_list = Post.objects.filter(
-        category=category,                  # Принадлежит выбранной категории
-        is_published=True,                  # Публикация опубликована
-        pub_date__lte=timezone.now()        # Дата публикации не позже текущего времени
-    ).order_by('-pub_date')                 # Сортировка по убыванию даты
-    return render(request, 'blog/category.html', {
-        'category': category,
-        'post_list': post_list
-    })
+    """Отображение публикаций в категории"""
+    category = get_object_or_404(
+        Category,
+        slug=category_slug,
+        is_published=True)
+    posts = get_posts(
+        is_published=True,
+        category__is_published=True,
+        pub_date__lte=datetime.now(),
+        category=category)
+    page_obj = get_paginator(request, posts)
+    context = {'category': category,
+               'page_obj': page_obj}
+    return render(request, 'blog/post_list.html', context)
+
+
+def post_detail(request, post_id):
+    """Отображение полного описания выбранной публикации"""
+    post = get_object_or_404(Post, id=post_id)
+    if request.user != post.author:
+        post = get_object_or_404(
+            Post,
+            id=post_id,
+            is_published=True,
+            category__is_published=True,
+            pub_date__lte=datetime.now())
+    form = CommentForm(request.POST or None)
+    comments = Comment.objects.select_related(
+        'author').filter(post=post)
+    context = {'post': post,
+               'form': form,
+               'comments': comments}
+    return render(request, 'blog/post_detail.html', context)
+
+
+@login_required
+def create_post(request):
+    """Создание публикации"""
+    form = PostForm(request.POST or None, files=request.FILES or None)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+        return redirect('blog:profile', request.user)
+    context = {'form': form}
+    return render(request, 'blog/create.html', context)
+
+
+@login_required
+def edit_post(request, post_id):
+    """Редактирование публикации"""
+    post = get_object_or_404(Post, id=post_id)
+    if request.user != post.author:
+        return redirect('blog:post_detail', post_id)
+    form = PostForm(request.POST or None, instance=post)
+    if form.is_valid():
+        form.save()
+        return redirect('blog:post_detail', post_id)
+    context = {'form': form}
+    return render(request, 'blog/create.html', context)
+
+
+@login_required
+def delete_post(request, post_id):
+    """Удаление публикации"""
+    post = get_object_or_404(Post, id=post_id)
+    if request.user != post.author:
+        return redirect('blog:post_detail', post_id)
+    form = PostForm(request.POST or None, instance=post)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('blog:index')
+    context = {'form': form}
+    return render(request, 'blog/create.html', context)
+
+
+@login_required
+def add_comment(request, post_id):
+    """Добавление комментария к публикации"""
+    post = get_object_or_404(Post, id=post_id)
+    form = CommentForm(request.POST or None)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.author = request.user
+        comment.post = post
+        comment.save()
+    return redirect('blog:post_detail', post_id)
+
+
+@login_required
+def edit_comment(request, post_id, comment_id):
+    """Редактирование комментария к публикации"""
+    comment = get_object_or_404(Comment, id=comment_id)
+    if request.user != comment.author:
+        return redirect('blog:post_detail', post_id)
+    form = CommentForm(request.POST or None, instance=comment)
+    if form.is_valid():
+        form.save()
+        return redirect('blog:post_detail', post_id)
+    context = {'comment': comment,
+               'form': form}
+    return render(request, 'blog/comment.html', context)
+
+
+@login_required
+def delete_comment(request, post_id, comment_id):
+    """Удаление комментария к публикации"""
+    comment = get_object_or_404(Comment, id=comment_id)
+    if request.user != comment.author:
+        return redirect('blog:post_detail', post_id)
+    if request.method == 'POST':
+        comment.delete()
+        return redirect('blog:post_detail', post_id)
+    context = {'comment': comment}
+    return render(request, 'blog/comment.html', context)
+
+
+def profile(request, username):
+    """Отображение страницы пользователя"""
+    profile = get_object_or_404(
+        User,
+        username=username)
+    posts = get_posts(author=profile)
+    if request.user != profile:
+        posts = get_posts(
+            is_published=True,
+            category__is_published=True,
+            pub_date__lte=datetime.now(),
+            author=profile)
+    page_obj = get_paginator(request, posts)
+    context = {'profile': profile,
+               'page_obj': page_obj}
+    return render(request, 'blog/profile.html', context)
+
+
+@login_required
+def edit_profile(request):
+    """Редактирование страницы пользователя"""
+    profile = get_object_or_404(
+        User,
+        username=request.user)
+    form = UserForm(request.POST or None, instance=profile)
+    if form.is_valid():
+        form.save()
+        return redirect('blog:profile', request.user)
+    context = {'form': form}
+    return render(request, 'blog/user.html', context)
